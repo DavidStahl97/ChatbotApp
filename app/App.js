a3be2cda8e4f67fcf1f7313bbc98fdb7e7d85650
de18702 <stahl.d@loh-services.de>
2020-06-10 17:31:46 +0200

    react-native-gifted-chat

--- app/App.js
+++ app/App.js
@@ -1,64 +1,47 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-import Config from 'react-native-config';
+import {GiftedChat} from 'react-native-gifted-chat';
 
 export default class MyApp extends Component {
   constructor() {
     super();
 
     this.state = {
-      number: 0,
-      answer: 'Hallo',
+      messages: [],
     };
   }
 
-  onPressButton() {
-    const newNumber = this.state.number + 1;
-    this.setState({number: newNumber});
-
-    const apiKey = Config.API_KEY;
-
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `EndpointKey ${apiKey}`,
-      },
-      body: JSON.stringify({
-        question: 'Ich habe Prüfungsangst, wie soll das vonstatten gehen?',
-      }),
-    };
-
-    fetch(
-      'https://qna-thm-service.azurewebsites.net/qnamaker/knowledgebases/bf3b2c41-215c-428b-9cff-1a11ea0a3789/generateAnswer',
-      requestOptions,
-    )
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        this.setState({answer: data.answers[0].answer});
-      });
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    });
   }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
   render() {
     return (
-      <View style={myStyles.myContainer}>
-        <Text style={myStyles.myText}>{'Hallo, drücke diesen Button'}</Text>
-        <Button title="Button" onPress={this.onPressButton.bind(this)} />
-        <Text>{this.state.number}</Text>
-        <Text>{this.state.answer}</Text>
-      </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
     );
   }
 }
-
-const myStyles = StyleSheet.create({
-  myContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCAA',
-  },
-  myText: {
-    fontSize: 50,
-  },
-});
