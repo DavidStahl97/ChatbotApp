744bf289582aae64f0365a2e2586364795ad15a2
de18702 <stahl.d@loh-services.de>
2020-06-15 20:37:15 +0200

    Implementing Navigation

--- app/App.js
+++ app/App.js
@@ -1,108 +1,18 @@
-import React, {Component} from 'react';
-import {GiftedChat} from 'react-native-gifted-chat';
-import uuid from 'react-native-uuid';
-import {answerAsync} from './api/BotAPI';
-import {retrieveItem, storeItem} from './api/StorageAPI';
-import {Appbar, Menu, Provider} from 'react-native-paper';
-import {View, StyleSheet} from 'react-native';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {ChatScreen} from './screens/chat';
 
-export default class MyApp extends Component {
-  messagesKey = 'messages';
+const Stack = createStackNavigator();
 
-  constructor() {
-    super();
-
-    this.state = {
-      messages: [],
-      visible: false,
-    };
-  }
-
-  componentDidMount() {
-    retrieveItem(this.messagesKey).then(messages => {
-      if (messages != null) {
-        this.setState(previousState => ({
-          messages: GiftedChat.append(previousState.messages, messages),
-        }));
-      }
-    });
-  }
-
-  onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
-
-    const question = messages[0].text;
-
-    answerAsync(question)
-      .then(answer =>
-        this.setState(previousState => ({
-          messages: GiftedChat.append(previousState.messages, {
-            _id: uuid.v1(),
-            text: answer,
-            createdAt: new Date(),
-            user: {
-              _id: 2,
-            },
-          }),
-        })),
-      )
-      .then(() => storeItem(this.messagesKey, this.state.messages));
-  }
-
-  _openMenu = () => this.setState({visible: true});
-
-  _closeMenu = () => this.setState({visible: false});
-
-  _deleteMessages = () => {
-    this.setState({messages: [], visible: false});
-    storeItem(this.messagesKey, []);
-  };
-
-  render() {
-    return (
-      <Provider>
-        <View style={appStyle.container}>
-          <Appbar.Header>
-            <Appbar.Content title="THM Assistent" />
-            <Menu
-              visible={this.state.visible}
-              onDismiss={this._closeMenu}
-              anchor={
-                <Appbar.Action
-                  icon="dots-vertical"
-                  onPress={this._openMenu}
-                  color="#fff"
-                />
-              }>
-              <Menu.Item onPress={this._deleteMessages} title="Löschen" />
-            </Menu>
-          </Appbar.Header>
-
-          <GiftedChat
-            messages={this.state.messages}
-            onSend={messages => this.onSend(messages)}
-            user={{
-              _id: 1,
-            }}
-          />
-        </View>
-      </Provider>
-    );
-  }
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="ChatScreen" component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-const appStyle = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  header: {
-    justifyContent: 'space-between',
-  },
-  headerTitle: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
+export default App;
---
+++ app/screens/chat.js
@@ -0,0 +1,109 @@
+import React, {Component} from 'react';
+import {GiftedChat} from 'react-native-gifted-chat';
+import uuid from 'react-native-uuid';
+import {answerAsync} from '../api/BotAPI';
+import {retrieveItem, storeItem} from '../api/StorageAPI';
+import {Appbar, Menu, Provider} from 'react-native-paper';
+import {View, StyleSheet} from 'react-native';
+
+export class ChatScreen extends Component {
+  messagesKey = 'messages';
+
+  constructor() {
+    super();
+
+    this.state = {
+      messages: [],
+      visible: false,
+    };
+  }
+
+  componentDidMount() {
+    retrieveItem(this.messagesKey).then(messages => {
+      if (messages != null) {
+        this.setState(previousState => ({
+          messages: GiftedChat.append(previousState.messages, messages),
+        }));
+      }
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+
+    const question = messages[0].text;
+
+    answerAsync(question)
+      .then(answer =>
+        this.setState(previousState => ({
+          messages: GiftedChat.append(previousState.messages, {
+            _id: uuid.v1(),
+            text: answer,
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+            },
+          }),
+        })),
+      )
+      .then(() => storeItem(this.messagesKey, this.state.messages));
+  }
+
+  _openMenu = () => this.setState({visible: true});
+
+  _closeMenu = () => this.setState({visible: false});
+
+  _deleteMessages = () => {
+    this.setState({messages: [], visible: false});
+    storeItem(this.messagesKey, []);
+  };
+
+  render() {
+    return (
+      <Provider>
+        <View style={appStyle.container}>
+          <Appbar.Header>
+            <Appbar.Content title="THM Assistent" />
+            <Menu
+              visible={this.state.visible}
+              onDismiss={this._closeMenu}
+              anchor={
+                <Appbar.Action
+                  icon="dots-vertical"
+                  onPress={this._openMenu}
+                  color="#fff"
+                />
+              }>
+              <Menu.Item onPress={this._deleteMessages} title="Löschen" />
+              <Menu.Item onPress={() => {}} title="Info" />
+            </Menu>
+          </Appbar.Header>
+
+          <GiftedChat
+            messages={this.state.messages}
+            onSend={messages => this.onSend(messages)}
+            user={{
+              _id: 1,
+            }}
+          />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const appStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    justifyContent: 'space-between',
+  },
+  headerTitle: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
